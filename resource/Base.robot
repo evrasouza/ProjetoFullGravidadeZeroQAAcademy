2c57ffee18ed75259dcd914e8127fedc902a40a0
Everton Souza <evrasouza@gmail.com>
2022-06-24 14:37:24 -0300

    Variáveis através da CLI do Robot

--- resource/Base.robot
+++ resource/Base.robot
@@ -17,7 +17,7 @@ ${URL_BASE}        https://getgeeks-evra.herokuapp.com
 
 *** Keywords ***
 Start Session
-    New Browser          chromium        headless=false        slowMo=00:00:00
+    New Browser          ${BROWSER}        headless=${HEADLESS}        slowMo=00:00:00
     New Page             ${URL_BASE}
 
     Set Viewport Size    1280    768
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ robot -l NONE -o NONE -r NONE tasks/Delorean.robot
 #robot -d ./logs -i rr_fields tests/Signup.robot
 #robot -d ./logs -i attempt_login tests/Login.robot
 #robot -d ./logs tests/BeGeek.robot
-robot -d ./logs tests/AttemptBeGeek.robot
+#robot -d ./logs tests/AttemptBeGeek.robot
 #robot -d ./logs -i smoke tests
 #robot -d ./logs  tests
+robot -d ./logs -v BROWSER:chromium -v HEADLESS:True -i smoke tests
